343d3fdb614ff947ed7567b03f963f296d9225c1
cindy21td <m.cindy.prasetio@gmail.com>
2015-08-04 14:44:31 -0700

    Added code for location detection

--- Hackjak/Eclair/app/src/main/java/com/hackjak/eclair/eclair/MapsActivity.java
+++ Hackjak/Eclair/app/src/main/java/com/hackjak/eclair/eclair/MapsActivity.java
@@ -1,8 +1,16 @@
 package com.hackjak.eclair.eclair;
 
+import android.location.Location;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -10,15 +18,33 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity {
+public class MapsActivity extends FragmentActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+
+    protected static final String TAG = "location-updates";
+    public static final long UPDATE_INTERVAL_IN_MILLISECONDS = 10000;
+    public static final long FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS =
+            UPDATE_INTERVAL_IN_MILLISECONDS / 2;
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+
+    private GoogleApiClient mGoogleApiClient;
+    protected LocationRequest mLocationRequest;
+
+    private Location mCurrentLocation;
+
+
+    protected Boolean mRequestingLocationUpdates;
+    protected String mLastUpdateTime;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         setUpMapIfNeeded();
+
+        buildGoogleApiClient();
     }
 
     @Override
@@ -52,14 +78,41 @@ public class MapsActivity extends FragmentActivity {
             if (mMap != null) {
                 setUpMap();
 
+                // Set Camera
                 CameraPosition cameraPosition = new CameraPosition.Builder()
                         .target(new LatLng(-6.175387, 106.827153))      // Sets the center of the map to Mountain View
                         .zoom(17)                   // Sets the zoom
                         .tilt(75)                   // Sets the tilt of the camera to 30 degrees
                         .build();                   // Creates a CameraPosition from the builder
                 mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+                mMap.setMyLocationEnabled(true);
+
+                mMap.setOnMyLocationButtonClickListener(new GoogleMap.OnMyLocationButtonClickListener() {
+                    @Override
+                    public boolean onMyLocationButtonClick() {
+                        //Toast.makeText(
+                        //        getApplicationContext(),
+                        //        "Example de Message for Android",
+                        //        Toast.LENGTH_SHORT).show();
+
+                        if (!mRequestingLocationUpdates) {
+                            mRequestingLocationUpdates = true;
+                            startLocationUpdates();
+                        }
+                        return true;
+                    }
+                });
+
+
             }
         }
+    }
+
+    protected void startLocationUpdates() {
+        // The final argument to {@code requestLocationUpdates()} is a LocationListener
+        // (http://developer.android.com/reference/com/google/android/gms/location/LocationListener.html).
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
     }
 
     /**
@@ -70,5 +123,72 @@ public class MapsActivity extends FragmentActivity {
      */
     private void setUpMap() {
         mMap.addMarker(new MarkerOptions().position(new LatLng(-6.175387, 106.827153)).title("Marker"));
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        mCurrentLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+        if (mCurrentLocation != null) {
+            CameraUpdate center=
+                    CameraUpdateFactory.newLatLng(new LatLng(mCurrentLocation.getLatitude(),
+                           mCurrentLocation.getLongitude()));
+            mMap.moveCamera(center);
+            Toast.makeText(this, "Dice", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "No Dice", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i("Google API Location", "Connection suspended");
+        mGoogleApiClient.connect();
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        Log.i(TAG, "Building GoogleApiClient");
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        createLocationRequest();
+    }
+
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+
+        // Sets the desired interval for active location updates. This interval is
+        // inexact. You may not receive updates at all if no location sources are available, or
+        // you may receive them slower than requested. You may also receive updates faster than
+        // requested if other applications are requesting location at a faster interval.
+        mLocationRequest.setInterval(UPDATE_INTERVAL_IN_MILLISECONDS);
+
+        // Sets the fastest rate for active location updates. This interval is exact, and your
+        // application will never receive updates faster than this value.
+        mLocationRequest.setFastestInterval(FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS);
+
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.i("Google API Location", "Connection failed: ConnectionResult.getErrorCode() = " + connectionResult.getErrorCode());
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
     }
 }
